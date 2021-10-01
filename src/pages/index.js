fa899d7c267260853da51b7033d4912c11c0055d
Chris Dunne <chrisdunne66@gmail.com>
2021-10-01 18:58:05 +0100

    0.1 - On the way

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,228 +1,163 @@
 import * as React from "react"
+import './index.sass';
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Carousel from 'react-bootstrap/Carousel'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
-import { Button } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+import header from '../images/butterfly.png'
 
 // markup
 const IndexPage = () => {
+
+
+
+
+  
+
+
+ 
+
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <Carousel>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=First slide&bg=373940"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=Second slide&bg=282c34"
-      alt="Second slide"
-    />
 
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=Third slide&bg=20232a"
-      alt="Third slide"
-    />
+<>
+    <Container fluid className="navbarWrapper">
+      <Row>
+        <Col>NavBar</Col>
+      </Row>
+    </Container>
 
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
 
-      <Button>Button</Button>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+    <Container fluid className="header">
+      <Row className="text">
+        <Col>
+          <img src={header}></img>
+          
+          <div>
+            <h1>Artsy Furniture</h1>
+          </div>
+          
+        </Col>
+      </Row>
+    </Container>
+
+    <Container fluid="sm">
+       
+
+
+
+
+
+
+{/* Text 1 */}
+        <Row >
+          <Col md="12" >Text</Col>
+        </Row>
+{/* Card Section 1 */}
+        <Row>
+          <Col md="12">Card 1 - Full</Col>
+        </Row>
+
+        <Row md="8">
+          <Col xs={6}>Card 1  - Half</Col>
+          <Col xs={6}>Card 1 - Half</Col>
+        </Row>
+
+        <Row>
+          <Col>Card 1 - Full</Col>
+        </Row>
+
+{/* Text Section 2 */}
+        <Row >
+          <Col md="12" >Text</Col>
+        </Row>
+{/* Card Section 2 */}    
+        <Row>
+          <Col md="12">Card 2 - Full</Col>
+        </Row>
+
+        <Row md="12">
+          <Col xs={6}>Card 2 - Half</Col>
+          <Col xs={6}>Card 2 - Half</Col>
+        </Row>
+
+        <Row>
+          <Col md="12">Card 2 - Full</Col>
+        </Row>
+{/* Text Section 3 */}
+        <Row >
+          <Col md="12" >Text</Col>
+        </Row>
+
+{/* Card Section 3 */}
+        <Row md="12">
+          <Col xs={6}>Card 2 - Half</Col>
+          <Col xs={6}>Card 2 - Half</Col>
+        </Row>
+
+        <Row>
+          <Col md="12">Card 2 - Full</Col>
+        </Row>
+
+{/* Text Section 4 */}
+        <Row >
+          <Col md="12" >Text</Col>
+        </Row>
+
+{/* Card Section 4 */}
+        <Row md="12">
+          <Col xs={6}>Card 2 - Half</Col>
+          <Col xs={6}>Card 2 - Half</Col>
+        </Row>
+
+{/* Text Section 5 */}
+        <Row >
+          <Col md="12" >Text</Col>
+        </Row>
+
+{/* Footer */}
+
+
+{/* Footer Cards */}
+        <Row md="12">
+          <Col xs={3}>Card 2 - Half</Col>
+          <Col xs={3}>Card 2 - Half</Col>
+          <Col xs={3}>Card 2 - Half</Col>
+          <Col xs={3}>Card 2 - Half</Col>
+        </Row>
+
+        <Row >
+          <Col md="12" >Footer</Col>
+        </Row>
+
+
+
+
+        {/* <h1>Text Area 1</h1>
+
+        <h1>Card Block 2</h1>
+        <h1>Text Area 2</h1>
+
+        <h1>Card Block 3</h1>
+        <h1>Text Area 3</h1>
+
+        <h1>Footer Block</h1>
+          <h1>Footer Cards</h1>
+          <h1>Footer Text</h1> */}
+    
+      
+
+
+
+
+    </Container>
+    </>
   )
 }
 
