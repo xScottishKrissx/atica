0a4dfe4ad6bf293f9dbb9f67d33eb8474afee7f9
Chris Dunne <chrisdunne66@gmail.com>
2021-10-01 12:58:26 +0100

    Getting Setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import * as React from "react"
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Carousel from 'react-bootstrap/Carousel'
+
+import { Button } from 'react-bootstrap';
+
 // styles
 const pageStyles = {
   color: "#232129",
@@ -130,6 +136,45 @@ const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <title>Home Page</title>
+      <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/800x400?text=First slide&bg=373940"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/800x400?text=Second slide&bg=282c34"
+      alt="Second slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/800x400?text=Third slide&bg=20232a"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+
+      <Button>Button</Button>
       <h1 style={headingStyles}>
         Congratulations
         <br />
